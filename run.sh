0e5423cb060de5ee683d08b00d9462e5a6f96534
JonathansMoon <jonathanx56@hotmail.com>
2020-05-12 19:57:26 -0300

    adding docker and sh files and modifying env

---
+++ run.sh
@@ -0,0 +1,26 @@
+echo Uploading Application container
+docker-compose up -d
+
+echo Copying the configuration example file
+docker exec -it myproperty-app cp .env.example .env
+
+echo Install dependencies
+docker exec -it myproperty-app composer install
+
+echo Generate key
+docker exec -it myproperty-app php artisan key:generate
+
+echo Make migrations
+docker exec -it myproperty-app php artisan migrate
+
+echo Make seeds
+docker exec -it myproperty-app php artisan db:seed
+
+echo Generate jwt:secret
+docker exec -it myproperty-app php artisan jwt:secret
+
+echo config cache
+docker exec -it myproperty-app php artisan config:cache
+
+echo Information of new containers
+docker ps -a
